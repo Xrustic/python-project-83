d4fe4e7eb37c56a0d446de36f918af8596997d64
Sesiunina Olga <sesyunina.olga7@gmail.com>
2024-06-04 12:07:19 +0300

    fixed teacher check 1(5)

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -6,66 +6,57 @@ import datetime
 class DatabaseManager:
     database_url = None
 
-    def __init__(self, database_url):
-        self.database_url = database_url
+    def __init__(self, app):
+        self.app = app
 
-    # @staticmethod
-    # def execute_in_db(func):
-    #     """Декоратор для выполнения функций соединения с базой данных."""
-    #     def inner(*args, **kwargs):
-    #         with psycopg2.connect(args[0].app.config['DATABASE_URL']) as 
-    # conn:
-    #             with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-    #                 result = func(cursor=cursor, *args, **kwargs)
-    #                 conn.commit()
-    #                 return result
-    #     return inner
+    # def __connect(self, conn=None):
+    #     if conn is None:
+    #         conn = psycopg2.connect(self.database_url)
+    #         return conn
 
-    # @staticmethod
-    # def with_commit(func):
-    #     def inner(self, *args, **kwargs):
-    #         database = self.app.config['DATABASE_URL']
-    #         try:
-    #             with psycopg2.connect(database) as connection:
-    #                 cursor = connection.cursor(cursor_factory=
-    # NamedTupleCursor)
-    #                 result = func(self, cursor, *args, **kwargs)
-    #                 connection.commit()
-    #                 return result
-    #         except psycopg2.Error as e:
-    #             print(f'Ошибка при выполнении транзакции: {e}')
-    #             raise e
-    #     return inner
+    # def __do_insert(self, query, values, *args, conn=None):
+    #     with psycopg2.connect(args[0].app.config['DATABASE_URL']) as conn:
+    #         conn = self.__connect(conn)
+    #         with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+    #             cursor.execute(query, values)
+    #     conn.commit()
 
-    # @with_commit
-    # def insert_url(self, cursor, url):
-    #     date = datetime.date.today()
-    #     cursor.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)"
-    #                    "RETURNING *", (url, date))
-    #     url_data = cursor.fetchone()
-    #     return url_data
-    def __connect(self, conn=None):
-        if conn is None:
-            conn = psycopg2.connect(self.database_url)
-            return conn
+    # def __do_select(self, query, values=None, *args, conn=None):
+    #     with psycopg2.connect(args[0].app.config['DATABASE_URL']) as conn:
+    #         conn = self.__connect(conn)
+    #         with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+    #             cursor.execute(query, values)
+    #             result = cursor.fetchall()
+    #     return result
 
-    def __do_insert(self, query, values, *args, conn=None):
-        with psycopg2.connect(args[0].app.config['DATABASE_URL']) as conn:
-            conn = self.__connect(conn)
-            with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-                cursor.execute(query, values)
-        conn.commit()
+    @staticmethod
+    def execute_in_db(func):
+        """Декоратор для выполнения функций соединения с базой данных."""
+        def inner(*args, **kwargs):
+            with psycopg2.connect(args[0].app.config['DATABASE_URL']) as conn:
+                with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+                    result = func(cursor=cursor, *args, **kwargs)
+                    conn.commit()
+                    return result
+        return inner
 
-    def __do_select(self, query, values=None, *args, conn=None):
-        with psycopg2.connect(args[0].app.config['DATABASE_URL']) as conn:
-            conn = self.__connect(conn)
-            with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-                cursor.execute(query, values)
-                result = cursor.fetchall()
-        return result
+    @staticmethod
+    def with_commit(func):
+        def inner(self, *args, **kwargs):
+            try:
+                with psycopg2.connect(self.app.config['DATABASE_URL']) as conn:
+                    cursor = conn.cursor(cursor_factory=NamedTupleCursor)
+                    result = func(self, cursor, *args, **kwargs)
+                    conn.commit()
+                    return result
+            except psycopg2.Error as e:
+                print(f'Ошибка при выполнении транзакции: {e}')
+                raise e
+
+        return inner
 
     def add_url(self, url, conn=None):
-        conn = self.__connect(conn)
+        conn = self.execute_in_db(conn)
         result = self.find_urls_by_name(url, conn)
         if result:
             return result, False
@@ -79,7 +70,7 @@ class DatabaseManager:
         return result, True
 
     def find_urls_by_id(self, id, conn=None):
-        conn = self.__connect(conn)
+        conn = self.execute_in_db(conn)
         query = "SELECT * from urls WHERE id=%s"
         result = self.__do_select(query, (id,), conn)
         if result:
@@ -97,32 +88,6 @@ class DatabaseManager:
             result = result[0]
         return result
 
-    # def find_urls(self, id=None, name=None):
-    #     result = None
-    #     key, value = None, None
-    #     if id:
-    #         key, value = 'id', str(id)
-    #     elif name:
-    #         key, value = 'name', str(name)
-    #     if key:
-    #         conn = self.__connect()
-    #         with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-    #             curs.execute(f"SELECT * from urls WHERE {key}=%s", (value,))
-    #             result = curs.fetchall()
-    #     return result
-
-    # def find_one_url_by_id(self, id, conn=None):
-    #     result = self.find_urls_by_id(id, conn)
-    #     if result:
-    #         result = result[0]
-    #     return result
-
-    # def find_one_url_by_name(self, name, conn=None):
-    #     result = self.find_urls_by_name(name, conn)
-    #     if result:
-    #         result = result[0]
-    #     return result
-
     def get_all_url(self, conn=None):
         conn = self.__connect(conn)
         query = """SELECT ur.id, ur.name, ur.created_at,
@@ -135,7 +100,7 @@ class DatabaseManager:
         return result
 
     def add_check(self, url, result_check, conn=None):
-        conn = self.__connect(conn)
+        conn = self.execute_in_db(conn)
         current_date = datetime.datetime.now()
         url_item = self.find_urls_by_name(url, conn)
         if url_item:
@@ -155,7 +120,7 @@ class DatabaseManager:
         return True
 
     def find_checks_by_id(self, id, conn=None):
-        conn = self.__connect(conn)
+        conn = self.execute_in_db(conn)
         result = None
         value = str(id)
         if value:
